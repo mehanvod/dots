436b411590973f8ce5f96d47ddbef53edd3d453e
mehanvod <antoshkin_80@mail.ru>
2019-04-12 07:10:20 +0300

    2019.04.12 Update

--- .bin/berb.sh
+++ .bin/berb.sh
@@ -14,34 +14,34 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-R_DISK="sda1"
-B_DISK="sda2"
-H_DISK="sda3"
-S_DISK="sda4"
+Root_D="sda1"
+Boot_D="sda2"
+Swap_D="sda3"
+Home_D="sda4"
 
 loadkeys ru
 setfont cyr-sun16
 
 timedatectl set-ntp true
 
-mkfs.ext4 /dev/$R_DISK
+mkfs.ext4 /dev/$Root_D
 
-mkfs.ext2 /dev/$B_DISK -L boot
-# mkfs.fat -F32 /dev/$B_DISK -L boot
+mkfs.ext2 /dev/$Boot_D -L boot
+# mkfs.fat -F32 /dev/$Boot_D -L boot
 
-# mkfs.ext4 /dev/$H_DISK -L home
-mkswap /dev/$S_DISK -L swap
+mkswap /dev/$Swap_D -L swap
+# mkfs.ext4 /dev/$Home_D -L home
 
-mount /dev/$R_DISK /mnt
+mount /dev/$Root_D /mnt
 
 mkdir /mnt/{boot,home}
 # mkdir -p /mnt/{boot/efi,home}
 
-mount /dev/$B_DISK /mnt/boot
-# mount /dev/$B_DISK /mnt/boot/efi
+mount /dev/$Boot_D /mnt/boot
+# mount /dev/$Boot_D /mnt/boot/efi
 
-mount /dev/$H_DISK /mnt/home
-swapon /dev/$S_DISK
+mount /dev/$Home_D /mnt/home
+swapon /dev/$Swap_D
 
 # pacman -Sy --noconfirm --needed reflector
 # reflector -c "Russia" -c "Denmark" -f 5 -l 5 -p https -n 5 --save /etc/pacman.d/mirrorlist --sort rate
