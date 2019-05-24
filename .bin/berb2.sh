5e3076dff9709410197377f53da6b1d6fa627063
mehanvod <antoshkin_80@mail.ru>
2019-05-24 13:39:41 +0300

    2019.05.24 Update

--- .bin/berb2.sh
+++ .bin/berb2.sh
@@ -142,7 +142,10 @@ EOF
 
 echo 'include "/usr/share/nano/*.nanorc"' >> /etc/nanorc
 echo 'QT_QPA_PLATFORMTHEME=qt5ct' >> /etc/environment
+echo 'vm.swappiness=10' >> /etc/sysctl.d/99-sysctl.conf
 sed -i 's/#export FREETYPE_PROPERTIES="truetype:interpreter-version=40"/export FREETYPE_PROPERTIES="truetype:interpreter-version=38"/g' /etc/profile.d/freetype2.sh
+sed -i 's/MODULES=()/MODULES=(amdgpu)/g' /etc/mkinitcpio.conf
+sed -i 's/#SystemMaxUse=/SystemMaxUse=5M/g' /etc/systemd/journald.conf
 
 # systemctl enable NetworkManager
 systemctl enable lightdm netctl dhcpcd
