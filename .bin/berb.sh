53f814738c9e2ce6ad37337b96b2c1fb18b121db
mehanvod <antoshkin_80@mail.ru>
2019-12-24 09:07:31 +0300

    2019.12.24 Update :metal:

--- .bin/berb.sh
+++ .bin/berb.sh
@@ -29,9 +29,14 @@ mount /dev/$Root_D /mnt
 
 ## boot ##
 # mkfs.ext2 /dev/$Boot_D -L boot
-mkfs.vfat -F32 -n "Boot" /dev/$Boot_D
+# mkfs.vfat -F32 -n "Boot" /dev/$Boot_D
 mkdir -p /mnt/boot
 mount /dev/$Boot_D /mnt/boot
+cd /mnt
+ls | grep -v boot | xargs rm -rfv
+cd /mnt/boot/EFI
+ls | grep -v Boot | grep -v Microsoft | xargs rm -rfv
+cd /root
 
 ## home ##
 # mkfs.ext4 -L "Home" /dev/$Home_D
